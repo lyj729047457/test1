caa13f0d4f2de55d1d7f30ae89486c80ffe81ee7
ster <lyj5039226@126.com>
2020-03-04 16:19:38 +0800

    commit1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate curl;
 fn main(){
-    println!("HW");
+    use std::io::{stdout, Write};
+
+    use curl::easy::Easy;
+
+// Write the contents of rust-lang.org to stdout
+    let mut easy = Easy::new();
+    easy.url("https://www.rust-lang.org/").unwrap();
+    easy.write_function(|data| {
+        Ok(stdout().write(data).unwrap())
+    }).unwrap();
+    easy.perform().unwrap();
 }
